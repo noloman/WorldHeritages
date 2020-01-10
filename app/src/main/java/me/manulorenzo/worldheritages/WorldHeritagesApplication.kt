456f3350e7ad7f200545a56448fc4ff9bc699807
Manuel Lorenzo <manulorenzop@gmail.com>
2020-01-10 11:36:05 +0100

    set StrictMode

--- app/src/main/java/me/manulorenzo/worldheritages/WorldHeritagesApplication.kt
+++ app/src/main/java/me/manulorenzo/worldheritages/WorldHeritagesApplication.kt
@@ -1,6 +1,7 @@
 package me.manulorenzo.worldheritages
 
 import android.app.Application
+import android.os.StrictMode
 import me.manulorenzo.worldheritages.data.di.assetManagerModule
 import me.manulorenzo.worldheritages.data.di.repositoryModule
 import me.manulorenzo.worldheritages.ui.main.di.mainViewModelModule
@@ -12,6 +13,18 @@ import org.koin.core.context.startKoin
 class WorldHeritagesApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+        StrictMode.setThreadPolicy(
+            StrictMode.ThreadPolicy.Builder()
+                .detectAll()
+                .penaltyLog()
+                .build()
+        )
+        StrictMode.setVmPolicy(
+            StrictMode.VmPolicy.Builder()
+                .detectAll()
+                .penaltyDeath()
+                .build()
+        )
         startKoin {
             androidLogger()
             androidContext(this@WorldHeritagesApplication)
